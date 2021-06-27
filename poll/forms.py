141f6abc3ad7a63093c788bfdc86d6e4fe6afb8e
Bino Maiheu <bino.maiheu@vito.be>
2021-06-27 18:45:20 +0200

    added form with titles

--- poll/forms.py
+++ poll/forms.py
@@ -1,8 +1,26 @@
 from flask_wtf import FlaskForm
 from flask_wtf.recaptcha import validators
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms import FieldList, FormField
+from wtforms.validators import DataRequired, InputRequired, NumberRange
 
 class LoginForm(FlaskForm):
     secret_key = StringField('secret_key', validators=[DataRequired()])
     submit = SubmitField("Verzenden")
+
+
+class QuestionForm(FlaskForm):
+    """
+    Form for an individual question with a rating
+    """    
+    rating = IntegerField('rating', validators=[InputRequired(), NumberRange(min=0, max=5)])
+
+
+class PollForm(FlaskForm):
+    """
+    Form for a list of questions, see : https://newbedev.com/add-input-fields-dynamically-with-wtforms
+
+    """
+    questions = FieldList(FormField(QuestionForm), min_entries=1)
+    submit = SubmitField("Verstuur")
+
--- poll/main.py
+++ poll/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from flask_login.utils import _secret_key
 from . import db
+from .forms import PollForm
 
 main = Blueprint('main', __name__)
 
@@ -10,10 +11,40 @@ def index():
     return render_template('index.html')
 
 
-@main.route('/poll')
+@main.route('/poll', methods=['GET', 'POST'])
 @login_required
-def poll():    
+def poll(): 
+
+    # https://prettyprinted.com/tutorials/how-to-use-fieldlist-in-flask-wtf
+   
+
+    titles = [
+        "Idea 1",
+        "Idea 2",
+        "Idea 3"
+    ]
+
+
+    form = PollForm(questions=[ 
+        {"rating": 5 },
+        {"rating": 3 },
+        {"rating": 0 } ])
+   
+    print("about to validate... ")
+
+    if form.validate_on_submit():  # we don't have to check whether it is a POST request, validate_on_submit does this
+        print("here !!")
+        for rating in form.questions.data:
+            print(rating)
+
+        # here redirect to "thank you page" return redirect(url_for("main.poll"))  
+
+    print(form.questions)
+    print(form.errors)
+
     return render_template(
         'poll.html',
-        secret_key=current_user.secret_key)
+        secret_key=current_user.secret_key, 
+        titles=titles,    
+        form=form)
 
